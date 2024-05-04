8808dfb3850450f9091d1b4498821c9e236fa931
Thomas Carson <thomas.carson2024@gmail.com>
2024-05-04 22:21:14 +0100

    Before Linked List removal (it doesn't work)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
-typedef struct LLNode
+struct LLNode
 {
     string key = key;
     string value = value;
     LLNode *next = nullptr;
+    LLNode()
+    {
+        this->key = "";
+        this->value = "";
+        cout << "Default constructor called" << endl;
+    }
     LLNode(string key, string value)
     {
         this->key = key;
@@ -21,26 +30,87 @@ typedef struct LLNode
         prev->next = this;
     }
 };
-
-long long Hash(string input)
+unsigned long long Hash(string input)
 {
-    long long result = 1;
+    unsigned long long result = 1;
     for (int i = 0; i < input.length(); i++)
     {
         result += input[i] * (i + 1);
     }
     return result;
 }
+class Dict
+{
+public:
+    int size;
+    vector<LLNode> vec;
+    Dict(string key, string value, int size)
+    {
+        this->size = size;
+        vec.resize(size * sizeof(LLNode));
+        int index = Hash(key) % size;
+        if (vec[index].key == "")
+        {
+            vec[index].key = key;
+            vec[index].value = value;
+        }
+        else
+        {
+            new LLNode(key, value, &vec[index]);
+        }
+    }
+    string Get(string key)
+    {
+        int index = Hash(key) % size;
+        if (vec[index].key == key)
+        {
+            return vec[index].value;
+        }
+        else
+        {
+            return "";
+        }
+    }
+    void Set(string key, string value)
+    {
+        int index = Hash(key) % size;
+        if (vec[index].key == key)
+        {
+            vec[index].value = value;
+        }
+        else
+        {
+            new LLNode(key, value, &vec[index]);
+        }
+    }
+};
 
 int main()
 {
+    int len;
+    cout << "Enter length of HashMap: ";
+    cin >> len;
+    string key, value;
+    cout << "Enter key: ";
+    cin >> key;
+    cout << "Enter value: ";
+    cin >> value;
+    Dict dict(key, value, len);
     while (true)
     {
-        cout << "Enter a string: ";
         string input;
+        cout << "Enter a key: ";
         cin >> input;
-        cout << "Hash: ";
-        cout << Hash(input) << endl;
+        string result = dict.Get(input);
+        if (!result.empty())
+        {
+            cout << "Value: " << result << endl;
+        }
+        else
+        {
+            cout << "Enter a value:";
+            cin >> value;
+        }
     }
     return 0;
 }
